34554498e00ed384bb362cec049b378c1c0fd672
eevelweezel <eevel.weezel@gmail.com>
2026-07-22 21:01:31 -0500

    fix form, model, add bootstrap

--- lawfirm/models.py
+++ lawfirm/models.py
@@ -3,5 +3,8 @@ from django.db import models
 
 class Section(models.Model):
     published = models.BooleanField()
+    slug = models.SlugField()
     title = models.CharField(max_length=150)
+    link_text = models.CharField(max_length=64)
     body = models.CharField(max_length=3000)
+    image = models.ImageField()
--- lawfirm/views.py
+++ lawfirm/views.py
@@ -1,7 +1,9 @@
+from django.contrib import messages
 from django.core.mail import EmailMessage
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 
+from . import forms
 
 class Home(TemplateView):
     template_view = "home.html"
@@ -14,4 +16,31 @@ class Home(TemplateView):
     def get(self, request, *args, **kwargs):
         context = self.get_context_view(*args, **kwargs)
         content = self.template_view.render(context)
-        return HttpResponse(content, status_code=200)  
+        return HttpResponse(content, status_code=200)
+
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_view(*args, **kwargs)
+        content = self.template_view.render(context)
+        form = forms.Contact(request.POST)
+        if form.is_valid():
+            # Send an email & display "SUCCESS" message inline
+            subject = form.cleaned_data["subject"]
+            message = form.cleaned_data["message"]
+            contact_email = form.cleaned_data["contact_email"]
+
+            body = (
+                "Submitted through the contact form.\n"
+                f"Contact email (unverified): {contact_email}\n"
+                f"Message:\n{message}"
+            )
+
+            EmailMessage(
+                subject=f"[contact form] {subject}",
+                body=body,
+                from_email="contact-form@example.com",
+                to=["info@example.com"],
+                reply_to=[contact_email],
+            ).send()
+
+            messages.success(request, "Thanks! We'll get back to you shortly!")
+        return HttpResponse(content, status_code=200)
